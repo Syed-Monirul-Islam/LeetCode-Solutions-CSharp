40734e1be1c687d560c1eac59d806f27492b877b
Moni <syedmonirulislamcu@gmail.com>
2026-08-18 18:01:07 +0600

    Add P0001 Two Sum solution and setup Program.cs

---
+++ LeetCodeSolutions/Arrays/P0001_TwoSum.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions.Arrays
+{
+    public class P0001_TwoSum
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var map = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+
+                if (map.ContainsKey(complement))
+                {
+                    return new int[] { map[complement], i };
+                }
+
+                if (!map.ContainsKey(nums[i]))
+                {
+                    map.Add(nums[i], i);
+                }
+            }
+
+            return Array.Empty<int>();
+        }
+    }
+}
---
+++ LeetCodeSolutions/Program.cs
@@ -0,0 +1,18 @@
+﻿using LeetCodeSolutions.Arrays;
+
+namespace LeetCodeSolutions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // LeetCode Solutions in C#
+            // Check the folder-wise problem files for solutions.
+            Console.WriteLine("LeetCode Solutions Repository");
+
+            // Temporary test space
+
+            Console.ReadLine();
+        }
+    }
+}
