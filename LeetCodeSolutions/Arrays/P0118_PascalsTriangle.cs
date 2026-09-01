c96e9bdb50d9e32f065cc1b82aec16365c6c71b4
Moni <syedmonirulislamcu@gmail.com>
2026-09-01 19:32:18 +0600

    Add LeetCode P0118 Pascal's Triangle solution

---
+++ LeetCodeSolutions/Arrays/P0118_PascalsTriangle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions.Arrays
+{
+    public class P0118_PascalsTriangle
+    {
+        public IList<IList<int>> Generate(int numRows)
+        {
+            IList<IList<int>> triangle = new List<IList<int>>();
+
+            for (int i = 0; i < numRows; i++)
+            {
+                List<int> row = new List<int>();
+
+                for (int j = 0; j <= i; j++)
+                {
+                    
+                    if (j == 0 || j == i)
+                    {
+                        row.Add(1);
+                    }
+                    else
+                    {
+                    
+                        int val = triangle[i - 1][j - 1] + triangle[i - 1][j];
+                        row.Add(val);
+                    }
+                }
+
+                triangle.Add(row);
+            }
+
+            return triangle;
+        }
+    }
+}
