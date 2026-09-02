47fedac822ca3d75a3b74f43c029290017d5a644
Moni <syedmonirulislamcu@gmail.com>
2026-09-02 19:50:53 +0600

    Add LeetCode P0119 Pascal's Triangle II solution

---
+++ LeetCodeSolutions/Arrays/P0119_PascalsTriangleII.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions.Arrays
+{
+    public class P0119_PascalsTriangleII
+    {
+        public IList<int> GetRow(int rowIndex)
+        {
+            List<int> row = new List<int>();
+
+            for (int i = 0; i <= rowIndex; i++)
+            {
+                
+                row.Add(1);
+
+                
+                for (int j = i - 1; j > 0; j--)
+                {
+                    row[j] = row[j] + row[j - 1];
+                }
+            }
+
+            return row;
+        }
+    }
+}
