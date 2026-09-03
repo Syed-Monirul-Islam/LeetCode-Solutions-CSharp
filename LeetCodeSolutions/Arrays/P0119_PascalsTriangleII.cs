1a857646b4725d9698e87f42b1e1c96b5d0084e2
Moni <syedmonirulislamcu@gmail.com>
2026-09-03 22:04:18 +0600

    Add LeetCode P0136 Single Number solution

--- LeetCodeSolutions/Arrays/P0119_PascalsTriangleII.cs
+++ LeetCodeSolutions/Arrays/P0119_PascalsTriangleII.cs
@@ -16,8 +16,7 @@ namespace LeetCodeSolutions.Arrays
             {
                 
                 row.Add(1);
-
-                
+            
                 for (int j = i - 1; j > 0; j--)
                 {
                     row[j] = row[j] + row[j - 1];
---
+++ LeetCodeSolutions/Arrays/P0136_SingleNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions.Arrays
+{
+    public class P0136_SingleNumber
+    {
+        public int SingleNumber(int[] nums)
+        {
+            int result = 0;
+
+            foreach (int num in nums)
+            {
+                result ^= num;
+            }
+
+            return result;
+        }
+    }
+}
