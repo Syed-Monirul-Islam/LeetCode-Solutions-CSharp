90276635be79ebee9c9b55a2b4abfdff03634ee9
Moni <syedmonirulislamcu@gmail.com>
2026-09-04 18:48:38 +0600

    Add LeetCode P0217 Contains Duplicate solution

---
+++ LeetCodeSolutions/Arrays/P0217_ContainsDuplicate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions.Arrays
+{
+    public class P0217_ContainsDuplicate
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            HashSet<int> seen = new HashSet<int>();
+
+            foreach (int num in nums)
+            {
+               
+                if (!seen.Add(num))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
