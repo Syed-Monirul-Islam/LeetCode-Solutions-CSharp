f5e80316c19735d31a900e77bafbc8306746f499
Moni <syedmonirulislamcu@gmail.com>
2026-09-05 22:57:01 +0600

    Add LeetCode P0137 Single Number II sorting solution

---
+++ LeetCodeSolutions/Arrays/P0137_SingleNumberII.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions.Arrays
+{
+    public class P0137_SingleNumberII
+    {
+        public int SingleNumber(int[] nums)
+        {
+            Array.Sort(nums);
+
+            
+            for (int i = 0; i < nums.Length - 1; i += 3)
+            {
+               
+                if (nums[i] != nums[i + 1])
+                {
+                    return nums[i];
+                }
+            }
+
+            
+            return nums[nums.Length - 1];
+        }
+    }
+}
