d35bcae90f4632f8a2d6043cdc172fda83520d3a
Moni <syedmonirulislamcu@gmail.com>
2026-09-21 17:57:27 +0600

    Add LeetCode P0067 Add Binary simulation solution

---
+++ LeetCodeSolutions/Math/P0067_AddBinary.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions.Math
+{
+    public class P0067_AddBinary
+    {
+        public string AddBinary(string a, string b)
+        {
+            StringBuilder sb = new StringBuilder();
+            int i = a.Length - 1;
+            int j = b.Length - 1;
+            int carry = 0;
+
+            
+            while (i >= 0 || j >= 0 || carry > 0)
+            {
+                int sum = carry;
+
+                if (i >= 0)
+                {
+                    sum += a[i] - '0'; 
+                    i--;
+                }
+
+                if (j >= 0)
+                {
+                    sum += b[j] - '0';
+                    j--;
+                }
+
+                
+                sb.Append(sum % 2);
+
+                
+                carry = sum / 2;
+            }
+
+            
+            char[] charArray = sb.ToString().ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
