1f32c160ac21fec27018e0a13a31c7d69d649f4b
Moni <syedmonirulislamcu@gmail.com>
2026-09-22 15:12:47 +0600

    Add LeetCode P0171 Excel Sheet Column Number base-26 solution

---
+++ LeetCodeSolutions/Math/P0171_ExcelSheetColumnNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions.Math
+{
+    public class P0171_ExcelSheetColumnNumber
+    {
+        public int TitleToNumber(string columnTitle)
+        {
+            int result = 0;
+
+            foreach (char c in columnTitle)
+            {
+                
+                int value = c - 'A' + 1;
+
+               
+                result = (result * 26) + value;
+            }
+
+            return result;
+        }
+    }
+}
